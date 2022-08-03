9161a6732aa1176d9b0aedfd2984a4a98d8c3e1d
NursaidNart <nursaidnart@gmail.com>
2022-08-03 04:11:26 +0300

    #2 initialtion vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,26 @@
 import './bootstrap';
 import '../sass/app.scss'
 
+
+
+import App from './components/App.vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import { routes } from './routes';
+import * as VueRouter from 'vue-router';
+import * as Vue from 'vue'
+
+
+
+
+const router =  VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes: routes
+});
+
+
+const app = Vue.createApp(App);
+app.use(router)
+app.use(VueAxios, axios)
+
+app.mount('#app');
